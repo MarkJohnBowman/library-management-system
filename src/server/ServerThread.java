c4713825a50e75f084898bd4d7cd6aeb004298c6
MarkJohnBowman <markjohnbowman223@gmail.com>
2025-12-22 17:05:56 +0000

    Create ServerThread skeleton for handling client connections

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -1,5 +1,34 @@
 package server;
 
-public class ServerThread {
+import java.net.Socket;
+import java.util.List;
 
+import models.LibraryRecord;
+import models.User;
+
+/**
+ * ServerThread - Handles individual client connections
+ * Each client gets its own thread
+ * 
+ * @author Mark Bowman
+ * @student G00389705
+ */
+public class ServerThread extends Thread {
+    
+    private Socket socket;
+    private List<User> registeredUsers;
+    private List<LibraryRecord> libraryRecords;
+    
+    public ServerThread(Socket socket, List<User> users, List<LibraryRecord> records) {
+        this.socket = socket;
+        this.registeredUsers = users;
+        this.libraryRecords = records;
+    }
+    
+    @Override
+    public void run() {
+        System.out.println("ServerThread started for client: " + socket.getInetAddress().getHostAddress());
+        
+        
+    }
 }
