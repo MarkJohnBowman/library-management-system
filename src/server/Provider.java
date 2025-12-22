f9afbbd4719f5b0d9caabd702db0bdf129c72091
MarkJohnBowman <markjohnbowman223@gmail.com>
2025-12-22 01:29:12 +0000

    Add ServerSocket and client connection acceptance

--- src/server/Provider.java
+++ src/server/Provider.java
@@ -2,6 +2,7 @@ package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Provider - Main server class
@@ -15,10 +16,32 @@ public class Provider {
     private static final int PORT = 2004;
     
     public static void main(String[] args) {
-        System.out.println("Library Management Server");
-        System.out.println("Port: " + PORT);
-        
-        // TODO: Initialize server socket
-        // TODO: Accept client connections
+    	Provider server = new Provider();
+        server.start();
     }
+    
+    public void start() {
+        System.out.println("===========================================");
+        System.out.println(" Library Management Server");
+        System.out.println(" Port: " + PORT);
+        System.out.println("===========================================");
+        
+        try {
+        	ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server started. Waiting for clients...");
+            
+            while (true) {
+                // Accept client connection
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+                
+                // TODO: Create thread for client
+                // TODO: Handle client communication
+            }
+            
+            } catch (IOException e) {
+                System.err.println("Server error: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
 }
