ee207c06dbe2ccac0e4fe9e5e4f97b2db5b7ac18
MarkJohnBowman <markjohnbowman223@gmail.com>
2025-12-22 01:23:27 +0000

    Create Provider server class skeleton

---
+++ src/server/Provider.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Provider - Main server class
+ * Listens for client connections on port 2004
+ * 
+ * @author Mark Bowman
+ * @student G00389705
+ */
+public class Provider {
+    
+    private static final int PORT = 2004;
+    
+    public static void main(String[] args) {
+        System.out.println("Library Management Server");
+        System.out.println("Port: " + PORT);
+        
+        // TODO: Initialize server socket
+        // TODO: Accept client connections
+    }
+}
